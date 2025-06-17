738ad32b117869da80344bf93cb94c342d912c03
Isaac <fimbresisaac23@gmail.com>
2025-06-17 01:51:58 -0700

    new search screen

---
+++ app/search.tsx
@@ -0,0 +1,62 @@
+import { ActivityIndicator, FlatList, StyleSheet, View } from 'react-native';
+
+import { PhotoCard } from '../components/PhotoCard';
+
+import { fetchAllPhotos } from '../state/slices/feed-slice';
+
+import { useBreakpoint } from '../hooks/use-breakpoint';
+import { useAppSelector } from '../hooks/use-app-selector';
+import { useAppDispatch } from '../hooks/use-app-dispatch';
+import { SearchHead } from '../components/SearchHead';
+
+export default function SearchScreen() {
+  const d = useBreakpoint();
+  const dispatch = useAppDispatch();
+  const { photos, hasMore, loading, searchQuery } = useAppSelector((s) => s.search);
+
+  const handleEndReached = () => {
+    if (!loading && hasMore) {
+      dispatch(fetchAllPhotos());
+    }
+  };
+  
+  return (
+    <FlatList
+        key={d === "mobile" ? "mobile" : "desktop"}
+        numColumns={d === "mobile" ? 2 : 3}
+        contentContainerStyle={styles.container}
+        columnWrapperStyle={styles.column}
+        data={photos}
+        extraData={loading}
+        onEndReached={handleEndReached}
+        onEndReachedThreshold={0.5}
+        ListFooterComponent={
+            loading ? (
+            <View style={styles.footer}>
+                <ActivityIndicator size="large" />
+            </View>
+            ) : null
+        }
+        ListHeaderComponent={() => <SearchHead />}
+        renderItem={({ item }) => <PhotoCard photo={item} />}
+    />
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+    gap: 10,
+    paddingHorizontal: 10,
+    paddingBottom: 60
+  },
+  column: {
+    gap: 10
+  },
+  footer: {
+    alignContent: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  }
+});
