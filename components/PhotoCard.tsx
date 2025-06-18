22e081bf16f6b07688541b1d58341e5408c4e388
Isaac <fimbresisaac23@gmail.com>
2025-06-17 23:29:16 -0700

    fixing fragment error

--- components/PhotoCard.tsx
+++ components/PhotoCard.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { View, Text, Image, StyleSheet, TouchableHighlight } from 'react-native';
+import { View, Text, Image, StyleSheet, Pressable } from 'react-native';
 import { useRouter } from 'expo-router';
 
 import { UnsplashPhoto } from '../types/unsplash';
@@ -13,22 +13,18 @@ export const PhotoCard: FC<PhotoCardProps> = ({ photo }) => {
   const router = useRouter();
 
   return (
-    <TouchableHighlight style={{ flex: 1 }} onPress={() => router.push(`/details/${photo.id}`)}>
-      <>
-        <Image
-          source={{
-            uri: photo.urls.thumb
-          }}
-          style={styles.thumbnail}
-        />
-        <View style={styles.container}>
-          <View>
-            <Text style={Styles.textTitleRegular} numberOfLines={1}>{photo.alt_description}</Text>
-            <Text style={Styles.textCaptionSmall} numberOfLines={1}>By {photo.user.name}</Text>
-          </View>
-        </View>
-      </>
-    </TouchableHighlight>
+    <Pressable style={{ flex: 1 }} onPress={() => router.push(`/details/${photo.id}`)}>
+      <Image
+        source={{
+          uri: photo.urls.thumb
+        }}
+        style={styles.thumbnail}
+      />
+      <View>
+        <Text style={Styles.textTitleRegular} numberOfLines={1}>{photo.alt_description}</Text>
+        <Text style={Styles.textCaptionSmall} numberOfLines={1}>By {photo.user.name}</Text>
+      </View>
+    </Pressable>
   )
 }
 
@@ -38,8 +34,5 @@ const styles = StyleSheet.create({
     aspectRatio: 1,
     borderRadius: 12,
     backgroundColor: '9e9e9e'
-  },
-  container: {
-
   },
 });
