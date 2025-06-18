0739e2fd86066806d55533749e9b8265368a2a62
Isaac <fimbresisaac23@gmail.com>
2025-06-17 17:00:04 -0700

    adding new colors

--- constants/colors.ts
+++ constants/colors.ts
@@ -1,9 +1,13 @@
 const black = '#020202';
 const border = '#f5f5f5';
 const placeholder = '#9e9e9e';
+const danger = '#E53935';
+const white = '#ffffff';
 
 export const Colors = {
   black,
   border,
   placeholder,
+  danger,
+  white,
 };
