620e1dcaeb07b795960e07839da391de5387b0ac
Isaac <fimbresisaac23@gmail.com>
2025-06-18 10:04:37 -0700

    new photos mock data

---
+++ mocks/photos.ts
@@ -0,0 +1,182 @@
+import { UnsplashPhoto } from "../types/unsplash";
+
+export const mockPhotos: UnsplashPhoto[] = [
+    {
+        id: "pG8C1dwigsI",
+        slug: "a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+        alternative_slugs: {
+            en: "a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+            es: "una-foto-en-blanco-y-negro-de-una-flor-en-un-estanque-pG8C1dwigsI",
+            ja: "池に咲く花のモノクロ写真-pG8C1dwigsI",
+            fr: "une-photo-en-noir-et-blanc-dune-fleur-dans-un-etang-pG8C1dwigsI",
+            it: "una-foto-in-bianco-e-nero-di-un-fiore-in-uno-stagno-pG8C1dwigsI",
+            ko: "연못에-있는-꽃의-흑백-사진-pG8C1dwigsI",
+            de: "ein-schwarz-weiss-foto-einer-blume-in-einem-teich-pG8C1dwigsI",
+            pt: "uma-foto-preto-e-branco-de-uma-flor-em-uma-lagoa-pG8C1dwigsI"
+        },
+        created_at: "2024-10-09T03:45:35Z",
+        updated_at: "2025-06-18T06:04:56Z",
+        promoted_at: null,
+        width: 2546,
+        height: 3792,
+        color: "#262626",
+        blur_hash: "L88z.G9F4nt7%MofM{Rj00xu_3IU",
+        description: null,
+        alt_description: "A black and white photo of a flower in a pond",
+        breadcrumbs: [],
+        urls: {
+            raw: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0",
+            full: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=85",
+            regular: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=80&w=1080",
+            small: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=80&w=400",
+            thumb: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=80&w=200",
+            small_s3: "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1728443783579-494fdbfd8512"
+        },
+        links: {
+            self: "https://api.unsplash.com/photos/a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+            html: "https://unsplash.com/photos/a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+            download: "https://unsplash.com/photos/pG8C1dwigsI/download?ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA",
+            download_location: "https://api.unsplash.com/photos/pG8C1dwigsI/download?ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA"
+        },
+        likes: 27,
+        liked_by_user: false,
+        current_user_collections: [],
+        sponsorship: null,
+        topic_submissions: {
+            film: {
+                status: "rejected"
+            }
+        },
+        asset_type: "photo",
+        user: {
+            id: "eteeO07lGPg",
+            updated_at: "2025-06-18T02:56:35Z",
+            username: "verrrdin",
+            name: "Antonio Verdín",
+            first_name: "Antonio",
+            last_name: "Verdín",
+            twitter_username: null,
+            portfolio_url: null,
+            bio: "I'm an independent Mexican photographer. I live and work in Guadalajara. In my photographic work, I explore the relationship between the everyday and youth, at the same time that I use my color blindness as a perceptive channel to produce images.",
+            location: "Mexico",
+            links: {
+                self: "https://api.unsplash.com/users/verrrdin",
+                html: "https://unsplash.com/@verrrdin",
+                photos: "https://api.unsplash.com/users/verrrdin/photos",
+                likes: "https://api.unsplash.com/users/verrrdin/likes",
+                portfolio: "https://api.unsplash.com/users/verrrdin/portfolio",
+                following: "",
+                followers: ""
+            },
+            profile_image: {
+                small: "https://images.unsplash.com/profile-1722905423134-a4016dee11e1image?ixlib=rb-4.1.0&crop=faces&fit=crop&w=32&h=32",
+                medium: "https://images.unsplash.com/profile-1722905423134-a4016dee11e1image?ixlib=rb-4.1.0&crop=faces&fit=crop&w=64&h=64",
+                large: "https://images.unsplash.com/profile-1722905423134-a4016dee11e1image?ixlib=rb-4.1.0&crop=faces&fit=crop&w=128&h=128"
+            },
+            instagram_username: "verrrdin",
+            total_collections: 0,
+            total_likes: 34,
+            total_photos: 300,
+            total_promoted_photos: 36,
+            total_illustrations: 0,
+            total_promoted_illustrations: 0,
+            accepted_tos: true,
+            for_hire: true,
+            social: {
+                instagram_username: "verrrdin",
+                portfolio_url: null,
+                twitter_username: null,
+                paypal_email: null
+            }
+        }
+    },
+        {
+        id: "pG8C1dwigsI",
+        slug: "a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+        alternative_slugs: {
+            en: "a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+            es: "una-foto-en-blanco-y-negro-de-una-flor-en-un-estanque-pG8C1dwigsI",
+            ja: "池に咲く花のモノクロ写真-pG8C1dwigsI",
+            fr: "une-photo-en-noir-et-blanc-dune-fleur-dans-un-etang-pG8C1dwigsI",
+            it: "una-foto-in-bianco-e-nero-di-un-fiore-in-uno-stagno-pG8C1dwigsI",
+            ko: "연못에-있는-꽃의-흑백-사진-pG8C1dwigsI",
+            de: "ein-schwarz-weiss-foto-einer-blume-in-einem-teich-pG8C1dwigsI",
+            pt: "uma-foto-preto-e-branco-de-uma-flor-em-uma-lagoa-pG8C1dwigsI"
+        },
+        created_at: "2024-10-09T03:45:35Z",
+        updated_at: "2025-06-18T06:04:56Z",
+        promoted_at: null,
+        width: 2546,
+        height: 3792,
+        color: "#262626",
+        blur_hash: "L88z.G9F4nt7%MofM{Rj00xu_3IU",
+        description: null,
+        alt_description: "A black and white photo of a flower in a pond",
+        breadcrumbs: [],
+        urls: {
+            raw: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0",
+            full: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=srgb&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=85",
+            regular: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=80&w=1080",
+            small: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=80&w=400",
+            thumb: "https://images.unsplash.com/photo-1728443783579-494fdbfd8512?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA&ixlib=rb-4.1.0&q=80&w=200",
+            small_s3: "https://s3.us-west-2.amazonaws.com/images.unsplash.com/small/photo-1728443783579-494fdbfd8512"
+        },
+        links: {
+            self: "https://api.unsplash.com/photos/a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+            html: "https://unsplash.com/photos/a-black-and-white-photo-of-a-flower-in-a-pond-pG8C1dwigsI",
+            download: "https://unsplash.com/photos/pG8C1dwigsI/download?ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA",
+            download_location: "https://api.unsplash.com/photos/pG8C1dwigsI/download?ixid=M3w3NjUwNTN8MHwxfGFsbHwxfHx8fHx8fHwxNzUwMjI5NzkzfA"
+        },
+        likes: 27,
+        liked_by_user: false,
+        current_user_collections: [],
+        sponsorship: null,
+        topic_submissions: {
+            film: {
+                status: "rejected"
+            }
+        },
+        asset_type: "photo",
+        user: {
+            id: "eteeO07lGPg",
+            updated_at: "2025-06-18T02:56:35Z",
+            username: "verrrdin",
+            name: "Antonio Verdín",
+            first_name: "Antonio",
+            last_name: "Verdín",
+            twitter_username: null,
+            portfolio_url: null,
+            bio: "I'm an independent Mexican photographer. I live and work in Guadalajara. In my photographic work, I explore the relationship between the everyday and youth, at the same time that I use my color blindness as a perceptive channel to produce images.",
+            location: "Mexico",
+            links: {
+                self: "https://api.unsplash.com/users/verrrdin",
+                html: "https://unsplash.com/@verrrdin",
+                photos: "https://api.unsplash.com/users/verrrdin/photos",
+                likes: "https://api.unsplash.com/users/verrrdin/likes",
+                portfolio: "https://api.unsplash.com/users/verrrdin/portfolio",
+                following: "",
+                followers: ""
+            },
+            profile_image: {
+                small: "https://images.unsplash.com/profile-1722905423134-a4016dee11e1image?ixlib=rb-4.1.0&crop=faces&fit=crop&w=32&h=32",
+                medium: "https://images.unsplash.com/profile-1722905423134-a4016dee11e1image?ixlib=rb-4.1.0&crop=faces&fit=crop&w=64&h=64",
+                large: "https://images.unsplash.com/profile-1722905423134-a4016dee11e1image?ixlib=rb-4.1.0&crop=faces&fit=crop&w=128&h=128"
+            },
+            instagram_username: "verrrdin",
+            total_collections: 0,
+            total_likes: 34,
+            total_photos: 300,
+            total_promoted_photos: 36,
+            total_illustrations: 0,
+            total_promoted_illustrations: 0,
+            accepted_tos: true,
+            for_hire: true,
+            social: {
+                instagram_username: "verrrdin",
+                portfolio_url: null,
+                twitter_username: null,
+                paypal_email: null
+            }
+        }
+    },
+];
